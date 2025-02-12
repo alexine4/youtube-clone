a4756c12e63bf568c98eeed38da8f14b3d6591ce
alexine4 <88242759+alexine4@users.noreply.github.com>
2025-02-12 21:57:11 +0200

    add var for change video size

--- frontend/youtube-clone-ui/src/app/video-player/video-player.component.ts
+++ frontend/youtube-clone-ui/src/app/video-player/video-player.component.ts
@@ -4,33 +4,37 @@ import { Component, Input, OnInit } from '@angular/core';
     selector: 'app-video-player',
     standalone: true,
     imports: [
-        CommonModule
+        CommonModule,
     ],
     templateUrl: './video-player.component.html',
     styleUrl: './video-player.component.scss'
 })
 export class VideoPlayerComponent implements OnInit {
  
+  @Input() size!: number | 0;
 
   @Input() videoUrl!: string | '';
-  @Input() width!: string | '';
 
   @Input() thumbnailUrl!: string | '';
 
   ngOnInit(): void {
     const video = document.getElementById("my_video_1")
     const aspectRatio = 16 / 10; // Або 4 / 3 для іншого формату
-
-    let newWidth = window.innerWidth * 0.5;
-    let newHeight = newWidth / aspectRatio;
   
+      
+     if (this.size !==0) {
+      let newWidth = window.innerWidth * 0.5;
+    let newHeight = newWidth / aspectRatio;
+    console.log(this.size);
     if (newHeight > window.innerHeight) {
-      newHeight = window.innerHeight *0.5;
+      newHeight = window.innerHeight * 0.5;
       newWidth = newHeight * aspectRatio;
     }
   
     video!.style.width = `${newWidth}px`;
     video!.style.height = `${newHeight}px`;
+    } 
+    
   }
 
 
